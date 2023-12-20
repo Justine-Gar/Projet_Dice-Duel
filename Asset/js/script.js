56008be263cb979d5cf77f9b8b12a4afc5c18f94
Justine Garandel <jg.garandel@hotmail.fr>
2023-12-20 17:54:53 +0100

    update js html css

--- Asset/js/script.js
+++ Asset/js/script.js
@@ -7,49 +7,78 @@
  * the game ends when one of the players reaches a score of 100.
  */
 
+//players
+const player1 = document.querySelector('.player1');
+const player2 = document.querySelector('.player2');
+//dice
 const dice = document.querySelector('.dice');
+//btns
 const rollBtn = document.querySelector('.rollBtn');
 const holdBtn = document.querySelector('.holdBtn');
-const newBtn = document.querySelector('.newBtn');
-const current1Score = document.getElementById('current1');
-const current2Score = document.getElementById('current2');
-const name1 = document.getElementById('name1');
-const name2 = document.getElementById('name2');
+//global score
 const globalScore1 = document.getElementById('globalScore1');
 const globalScore2 = document.getElementById('globalScore2');
-const player1 = document.getElementById('player1');
-const player2 = document.getElementById('player2');
+//current score
+const current1 = document.getElementById('current1');
+const current2 = document.getElementById('current2');
 
 
-let currentPlayer = 1;
-let current1PlayerScore = 0;
-let current2PlayerScore = 0;
+globalScore1.textContent = 0;
+globalScore2.textContent = 0;
 
-/*======New Game=====*/
+let scores = [0, 0];
+let currentScore = 0;
+let playerActive = 1;
+
+//switch
+function switchPlayer() {
+
+  document.getElementById(`current${playerActive}`).textContent = 0;
+  
+  if (playerActive === 1) {
+      playerActive = 2;
+  }else {
+      playerActive = 1;
+  }
+  currentScore = 0;
+  //déplace la class playeractive
+  player1.classList.toggle("playerActive");
+  player2.classList.toggle("playerActive");
+}
 
 
-/*======Dice Random=====*/
-const randomDice = () => {
+//==btn Roll==//
+function randomDice() {
 
+  let random;
+  do {
+    random = Math.floor(Math.random() * 6) + 1;
+  } while (random < 1 || random > 6);
+
+  rollDice(random);
+}
+/*function randomDice() {
+  //1.generer un random 
   const random = Math.floor(Math.random() * 6) + 1;
-
+  //2.condition/vérifié si nbr générer et entre 1 et 6
   if (random >= 1 && random <= 6) {
     rollDice(random);
   }
   else {
     randomDice();
   }
-}
+}*/
 
-const rollDice = (random) => {
+function rollDice (random) {
 
+  //debut animation dice
   dice.style.animation = 'rolling 4s';
   setTimeout(() => {
 
     switch (random) {
       case 1:
         dice.style.transform = 'rotateX(0deg) rotateY(0deg)';
-        current1PlayerScore = 0;
+        currentScore = 0;
         break;
       case 6:
         dice.style.transform = 'rotateX(180deg) rotateY(0deg)';
@@ -70,62 +99,40 @@ const rollDice = (random) => {
       default:
         break;
     }
-
-    dice.style.animation = 'none';
+    //arret animation dice
+    dice.style.animation = 'none'
 
     if (random !== 1) {
-
-      updateCurrentScore(random);
-
+      //score add
+      currentScore += random;
+      //current1.textContent = currentScore;
+      document.getElementById(`current${playerActive}`).textContent = currentScore;
     } else {
-
-      current1PlayerScore = 0;
-      current2PlayerScore = 0;
-      switchPlayer();
-
+      //change joueur 
+      switchPlayer()
     }
     
   }, 4050);
 }
 
-const updateCurrentScore = (value) => {
-  current1PlayerScore = value;
-  current1Score.textContent = current1PlayerScore;
-  current2PlayerScore = value;
-  current2Score.textContent = current2PlayerScore;
-}
-
-/*switch player */
-const switchPlayer = () => {
-
-  const playerActive = document.querySelector('.playerActive');
-  const currentScore = document.querySelector('.currentScore')
-  
-}
-
-/*hold score */
-const holdScore = () => {
-
-  if (currentPlayer === 1) {
-    player1Score += current1PlayerScore;
-    globalScore1.textContent = player1Score;
-  } else {
-
-  }
-  switchPlayer();
-}
-
-
 rollBtn.addEventListener('click', randomDice);
+
+
+//==btn Hold==//
+
+function holdScore() {
+  scores[playerActive] = parseInt(scores[playerActive], 10) || 0;
+  scores[playerActive] += currentScore;
+  //console.log(scores);
+  document.getElementById(`globalScore${playerActive}`).textContent = scores[playerActive];
+
+ if(scores[playerActive] >= 100) {
+
+ } else {
+  //switch de joueur
+  switchPlayer()
+ }
+
+}
+
 holdBtn.addEventListener('click', holdScore);
-
-
-
-
-
-/*======Score=======*/
-let player1Score = 0;
-let player2Score = 0;
-
-score1.textContent = 0;
-score2.textContent= 0;
